16edc1741c55427960e2bc52690856642ffa9043
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2019-11-25 09:58:52 +0100

    Init

---
+++ src/Controller/BezoekerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class BezoekerController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return $this->render('article/index.html.twig',['title' => ucwords(str_replace('-',' ', $slug))]);
+    }
+}
