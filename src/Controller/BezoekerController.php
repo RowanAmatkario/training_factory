ea8e5ec72b5d9d14b74c53a7b2723d7b2aea54e1
unknown <302506947@student.rocmondriaan.nl>
2019-11-25 20:48:34 +0100

    Homepage afgewerkt op school verder met database

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -15,6 +15,21 @@ class BezoekerController extends AbstractController
      */
     public function homepage()
     {
-        return $this->render('views/bezoeker/index.html.twig');
+        $comments = [
+            'Trainingscentrum Den Haag heeft ook een praktijk in Ypenburg. Het MTC Ypenburg.Na 10 jaar gezeten te hebben in het gezondheidscentrum Ypenburg zijn we op 3 juli 2017 overgestapt naar de Laan van Hoornwijck 174. Een uitbreiding van ons centrum met oefenzaal en fitness apparatuur.
+            In Ypenburg wordt er naast fysiotherapie ook Orthopedische Manuele Therapie, psychosomatische fysiotherapie, sportfysiotherapie, kinderfysiotherapie, oedeemtherapie en medische trainings therapie gegeven.
+            Het team van fysiotherapeuten bestaat uit zowel vrouwelijke als mannelijke therapeuten, die al geruime tijd in het vak zitten en zich regelmatig laten bij- en nascholen. De lijst van therapeuten die werkzaam zijn in Ypenburg vindt u hier.
+            Kwaliteit van zorg, overleg met (huis)artsen en multidisciplinair werken zijn zaken die het MTC Ypenburg belangrijk vindt.
+            Onze therapeuten staan geregistreerd in het Centraal Kwaliteits Register voor fysiotherapie en manuele therapie.',
+
+        ];
+        return $this->render('views/bezoeker/index.html.twig', [
+            'comments' => $comments,
+        ]);
+
     }
 }
+
+
+
+
