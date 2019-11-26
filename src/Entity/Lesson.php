85a22e3327b6431d7ddfc79f693b271a6ced97bf
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2019-11-26 10:26:19 +0100

    homepage afgewerkt, workspace map delete

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LessonRepository")
+ */
+class Lesson
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="time", nullable=true)
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $max_person;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(?\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getMaxPerson(): ?string
+    {
+        return $this->max_person;
+    }
+
+    public function setMaxPerson(string $max_person): self
+    {
+        $this->max_person = $max_person;
+
+        return $this;
+    }
+}
