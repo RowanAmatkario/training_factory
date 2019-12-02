df33e5d25136689e569b8b779f42da0fdbb1d76d
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2019-12-02 09:55:59 +0100

    button afgewerkt

--- src/Form/TrainingType.php
+++ src/Form/TrainingType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 
 use App\Entity\Training;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,8 @@ class TrainingType extends AbstractType
         $builder
             ->add('naam', TextType::class)
             ->add('description', TextType::class)
-            ->add('duration', TextType::class);
+            ->add('duration', TextType::class)
+            ->add('Toevoegen', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
