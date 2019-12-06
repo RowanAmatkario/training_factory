bd3f3fea18b1ebb51a6cf2f1ae738484ed07f054
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2019-12-06 13:02:45 +0100

    Bezig met login form

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('rowan.amatkario@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword (
+            $user,
+            'WelcomeNewMember'
+        ));
+        $manager->persist($user);
+        $user = new User();
+        $user->setEmail('vito.jeffrey@gmail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordEncoder->encodePassword (
+            $user,
+            'WelcomeNewMember'
+        ));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
---
+++ src/Migrations/Version20191206115537.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191206115537 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_34DCD1766D1A90C6 ON person (loginname)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_34DCD17635C246D5 ON person (password)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_34DCD1766D1A90C6 ON person');
+        $this->addSql('DROP INDEX UNIQ_34DCD17635C246D5 ON person');
+    }
+}
