29b314a2a1bd6fd84ebe426359858e6a62b713d4
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2020-01-07 15:13:46 +0100

    nieuwe punten afwerken

--- src/Controller/InstructeurController.php
+++ src/Controller/InstructeurController.php
@@ -7,7 +7,10 @@ namespace App\Controller;
 
 use App\Entity\Lesson;
 use App\Entity\Person;
+use App\Form\LessonType;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -19,12 +22,32 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class InstructeurController extends AbstractController
 {
     /**
-    * @Route("/planlessen", name="planlessen")
-    */
-    public function planlessenAction()
+     * @Route("/planlessen", name="planlessen")
+     */
+    public function new(Request $request)
     {
-        return $this->render('medewerker/planLes.html.twig');
+        $lesson = new Lesson();
+        $form = $this->createForm(LessonType::class, $lesson);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $task = $form->getData();
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+
+        return $this->render('medewerker/planLes.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
+
 
     /**
      * @Route("/lesBeheer", name="lesBeheer")
@@ -38,3 +61,4 @@ class InstructeurController extends AbstractController
     }
 
 }
+
