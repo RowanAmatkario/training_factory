1fbf9ca1469d1874b98d9a48943d02a31feda375
Rowan Amatkario <302506947@student.rocmondriaan.nl>
2020-01-09 10:26:24 +0100

    alle relaties afgewerkt

--- src/Migrations/Version20200109091115.php
+++ src/Migrations/Version20200109091115.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200109090629 extends AbstractMigration
+final class Version20200109091115 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,9 @@ final class Version20200109090629 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE registration ADD registration_id INT NOT NULL');
-        $this->addSql('ALTER TABLE registration ADD CONSTRAINT FK_62A8A7A7833D8F43 FOREIGN KEY (registration_id) REFERENCES lesson (id)');
-        $this->addSql('CREATE INDEX IDX_62A8A7A7833D8F43 ON registration (registration_id)');
+        $this->addSql('ALTER TABLE registration ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE registration ADD CONSTRAINT FK_62A8A7A7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_62A8A7A7A76ED395 ON registration (user_id)');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +32,8 @@ final class Version20200109090629 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A7833D8F43');
-        $this->addSql('DROP INDEX IDX_62A8A7A7833D8F43 ON registration');
-        $this->addSql('ALTER TABLE registration DROP registration_id');
+        $this->addSql('ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A7A76ED395');
+        $this->addSql('DROP INDEX IDX_62A8A7A7A76ED395 ON registration');
+        $this->addSql('ALTER TABLE registration DROP user_id');
     }
 }
